1522f9ce399ebc53fe392b17861ccbcacc88f986
James Rowe <jim@jimcode.org>
2012-12-10 15:26:50 -0500

    testin twitter bootstrap

--- application/views/shared/_header.php
+++ application/views/shared/_header.php
@@ -3,8 +3,8 @@
 <head>
     <title><?= $title ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
 </head>
 
 <body>
