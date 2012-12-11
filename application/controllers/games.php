70790476e98840b19e0fb39ebf510bbb5912a040
James Rowe <jim@jimcode.org>
2012-12-10 19:52:24 -0500

    typo fix

--- application/controllers/games.php
+++ application/controllers/games.php
@@ -21,7 +21,7 @@ class Games extends CI_Controller {
             'description' => 'Covert Mission is a group game with a star wars theme based around player deception and deduction of player motives, in the same genre as werewolf and mafia.'
         );
         
-        this->load->view('shared/_header.php', $this->view_data);
+        $this->load->view('shared/_header.php', $this->view_data);
         $this->load->view('games/new', $this->view_data);
         $this->load->view('shared/_footer.php', $this->view_data);
     }
