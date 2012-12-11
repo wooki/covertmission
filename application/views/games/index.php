2104444ae15330857addf84f40efa726ec28ff0e
James Rowe <jim@jimcode.org>
2012-12-11 17:44:52 -0500

    validation rules for create game

--- application/views/games/index.php
+++ application/views/games/index.php
@@ -3,8 +3,7 @@
     <div class="row">
     
         <div class="span12">
-            <h1>Please join a game</h1>
-            
+            <h1>Please join a game</h1>            
         </div>
         
     </div>
@@ -13,6 +12,7 @@
     
         <div class="span12 game-cmd">
             <?php foreach($games as $game) { 
+                echo "<p>".print_r($game, true)."</p>";
                 if ($game->state == 'joining') { ?>
             <a class="btn btn-primary btn-large" href="/"><?= $game->name ?></a>
             <?php }
