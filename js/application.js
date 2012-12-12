d99603019b4259f9ebd93386768ff9c26ac9057e
James Rowe <jim@jimcode.org>
2012-12-12 10:18:50 -0500

    ajax update lobby status

--- js/application.js
+++ js/application.js
@@ -1,28 +1,43 @@
-$(document).ready(function() {
+var covertmission = function() {
     
-    // set the background depending on player role
-    
-    $.backstretch("/img/rebels-bg.jpg");
-    
-    // lobby - refresh status every n seconds and update players
-    if ($('.lobby').length) {
-        // <p data-game-state="joining" class="alert alert-info game_state">Waiting to start</p>
-        setInterval(function() {
-            
-            $.ajax({
-               url: '/api/state/test',
-               error: function(xhr, textStatus, error) {
-                   $('.game_state').html('Error: '+textStatus);
-               },
-               success: function(data, textStatus, xhr) {
-                   var json_data = JSON.parse(data);
-                   $('.game_state').html(json_data['return']);                   
-               }
-            });
-            
-            $('.game_state')
-            
-        }, 3000);
+    // get a description of game statis
+    function get_state_description(state) {
+        if (state == 'joining') {
+            return 'Waiting to start';
+        } else {
+            return 'Unknown state: '+state;
+        }
     }
     
-});
+    // do things at startup
+    $(document).ready(function() {
+        
+        // set the background depending on player role
+        
+        $.backstretch("/img/rebels-bg.jpg");
+        
+        // lobby - refresh status every n seconds and update players
+        if ($('.lobby').length) {
+            // <p data-game-state="joining" class="alert alert-info game_state">Waiting to start</p>
+            setInterval(function() {
+                
+                $.ajax({
+                   url: '/api/state/test',
+                   error: function(xhr, textStatus, error) {
+                       $('.game_state').html('Error: '+textStatus);
+                   },
+                   success: function(data, textStatus, xhr) {
+                       var json_data = JSON.parse(data);
+                       $('.game_state').html(json_data['return']);                   
+                   }
+                });
+                
+                $('.game_state')
+                
+            }, 3000);
+        }
+        
+    });
+
+
+}();
