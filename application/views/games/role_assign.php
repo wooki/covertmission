eb8408537e5acde6220da1c878a87df2084df3fb
James Rowe <jim@jimcode.org>
2012-12-12 14:06:09 -0500

    start game page

--- application/views/games/role_assign.php
+++ application/views/games/role_assign.php
@@ -11,12 +11,17 @@
             
             <p data-game-state="joining" class="alert alert-info game_state">Starting game</p>
             
-            <p>You are playing as a<?php if ($player->role == "Imperial Officer") { echo 'n';  } ?> <strong><?= player_role($player->role) ?></strong>. You
+            <p>You are playing as a<?php if ($player->role == "Imperial Officer") { echo 'n';  } ?>
+            <strong><?= player_role($player->role) ?></strong>. You
             must complete 3 our of the 5 missions that you will be set to win the game.</p>
             
-            <p>A team leader will be assigned for each mission and they then select the operatives to take part in the mission. Not every player
-            will be sent on each mission. Once the mission team has been assembled all players vote to accept or reject the team. If the team
-            is rejected a new team leader will be selected and a new team assembled. If the mission is accepted then the operatives will attempt
+            <p>A team leader will be assigned for each mission and they then select the operatives to 
+            take part in the mission. Not every player
+            will be sent on each mission. Once the mission team has been assembled all players vote to 
+            accept or reject the team.</p>
+            
+            <p>If the team is rejected a new team leader will be selected and a new team assembled. If the 
+            mission is accepted then the operatives will attempt
             to complete the mission. Spies will try to saboutage the mission.</p>
             
             <p>Note that five rejected teams in a row will automatically lead to a win to the Rebel Spies.</p>
