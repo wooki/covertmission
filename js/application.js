f6e2504fb1953cbce26be5ad2695778600d6602b
James Rowe <jim@jimcode.org>
2012-12-12 10:08:01 -0500

    ajax update lobby status

--- js/application.js
+++ js/application.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
+    
+    // set the background depending on player role
+    
     $.backstretch("/img/rebels-bg.jpg");
+    
+    // lobby - refresh status every n seconds and update players
+    if ($('.lobby').length) {
+        // <p data-game-state="joining" class="alert alert-info game_state">Waiting to start</p>
+        setInterval(function() {
+            
+            $.ajax({
+               url: '/api/state/test',
+               error: function(xhr, textStatus, error) {
+                   alert('Error: '+textStatus);
+               },
+               success: function(data, textStatus, xhr) {
+                   alert('Success: '+textStatus+' : '+data);
+               }
+            });
+            
+            $('.game_state')
+            
+        }, 3000);
+    }
+    
 });
