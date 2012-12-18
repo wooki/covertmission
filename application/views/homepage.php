be4bdf229ff3d606dc3b3885d24f15531d12c5b9
James Rowe <jim@jimcode.org>
2012-12-18 06:58:58 -0500

    align buttons on homepage

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,48 +1,48 @@
 <div class="container page">
 
     <div class="row">
-    
+
         <div class="span12">
             <h1>Welcome to Covert Mission</h1>
         </div>
-        
+
     </div>
-    
+
     <?php if ($error != false) { echo '<div class="row"><div class="span4 offset4 game-cmd"><div class="alert alert-error">'.$error.'</div></div></div>'; } ?>
-    
+
     <div class="row">
-    
-        <div class="span3 offset2 game-cmd">
+
+        <div class="span1 offset4 game-cmd">
             <a class="btn btn-primary btn-large" href="/games/create">New Game</a>
         </div>
-        <div class="span3 offset2 game-cmd">
+        <div class="span1 offset2 game-cmd">
             <a class="btn btn-primary btn-large" href="/games">Join</a>
         </div>
-        
+
     </div>
 
     <div class="row">
-      
+
       <div class="span4 offset4">
-            
+
             <p>You will be set up to five missions to try and rid the galaxy of the Rebel Alliance.</p>
-            
+
             <p>Each mission will have a team leader who selects the mission team.  Missions have different
             team sizes.  If the mission team is not approved by a majority then a new mission leader is
             selected and may pick a new team.</p>
-            
+
             <p>Note that five rejected teams in a row will automatically lead to a win to the Rebel Spies.</p>
-            
-            <p>If the 
+
+            <p>If the
             mission team is approved then the Imperial operatives will attempt
             to complete the mission. Rebel Spies will try to saboutage the mission. This is done simply by
             voting for mission success or failure.</p>
-            
+
             <p>You must complete 3 our of the 5 missions that you will be set to win the game.</p>
-            
-            
+
+
         </div>
     </div>
-    
+
 </div>
 
