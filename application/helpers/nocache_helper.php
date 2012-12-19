9162e687c9af6678110710dae1c7422d75cac9cd
James Rowe <jim@jimcode.org>
2012-12-19 09:27:52 -0500

    prevent IE from caching the API call

--- application/helpers/nocache_helper.php
+++ application/helpers/nocache_helper.php
@@ -5,10 +5,10 @@ if ( ! function_exists('nocache')) {
     function nocache($output) {
         $output->set_header("HTTP/1.0 200 OK");
         $output->set_header("HTTP/1.1 200 OK");
-        $output->set_header('Expires: '.date('D, d M Y 05:00:00').' GMT');
+        $output->set_header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
         $output->set_header('Last-Modified: '.date('D, d M Y H:i:s').' GMT');
-        $output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
-        $output->set_header("Cache-Control: post-check=0, pre-check=0");
+        $output->set_header("Cache-Control: no-cache, no-store, must-revalidate, max-age=0");
+        $output->set_header("Cache-Control: post-check=0, pre-check=0", false);
         $output->set_header("Pragma: no-cache");
     }
 }
