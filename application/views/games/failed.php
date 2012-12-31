526a25e5aa811f82fa4f543a5c855fbdfa44af84
James Rowe <jim@jimcode.org>
2012-12-30 21:56:12 -0500

    fix 5 failed mission vites

--- application/views/games/failed.php
+++ application/views/games/failed.php
@@ -25,14 +25,9 @@
             <?php } ?>
             </div>
 
-            <?php if ($player->state == 'mission-result') {  ?>
-            <?= form_open('missions/result/'.$game->slug) ?>
-                <?= form_hidden('postback', 'acknowledge'); ?>
-                <button type="submit" class="btn btn-primary btn-large">Continue</button>
-            </form>
-            <?php } else {  ?>
-            <p>Please wait for all players</p>
-            <?php } ?>
+            <?= form_open('/') ?>
+            <button type="submit" class="btn btn-primary btn-large">Continue</button>
+            </form>            
         </div>
 
     </div>
